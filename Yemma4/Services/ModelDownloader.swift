a361cf65bd3cf1192f78e43c4287608822c370d0
jskoiz <jskoiz@users.noreply.github.com>
2026-04-03 11:00:17 -1000

    Polish UI and reduce launch overhead

--- Yemma4/Services/ModelDownloader.swift
+++ Yemma4/Services/ModelDownloader.swift
@@ -17,7 +17,6 @@ public final class ModelDownloader {
 
     public init(fileManager: FileManager = .default) {
         self.fileManager = fileManager
-        validateDownloadedModel()
     }
 
     public func validateDownloadedModel() {
